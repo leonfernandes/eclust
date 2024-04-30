744f30456ddc3419a97cc1784ddaa4fec845139d
LeonFernandes <leon.fernandes@columbia.edu>
2024-04-29 22:56:02 -0400

    fix gdp export

--- R/data.R
+++ R/data.R
@@ -1,7 +1,7 @@
 #' GDP Data of 23 Countries
 #'
 #' A subset of the Conference Board containing Real GDP of 23 most developed countries.
-#' @format `gdp`
+#' @format ## `gdp`
 #' A [tsibble::tsibble] of 920 rows and 3 columns:
 #' \describe{
 #'  \item{country}{Country name}
